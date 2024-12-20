3c3a2e491d1e50f81215be667bbfeaf85f101592
hardikmishra30 <mishrahardik3006@gmail.com>
2024-12-20 15:35:29 +0530

    List problem solved.

---
+++ DartProblems/list.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<int> num = [5, 3, 8, 1, 2];
+  num.add(7); //number 7 is addes using the add fucntion
+  print("$num");
+  num.sort(); //here the sort function is used to sort
+  print("$num");
+}
