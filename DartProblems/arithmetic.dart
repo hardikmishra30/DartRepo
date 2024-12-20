f68d3b9602df5a190681454b2df6b4c55f66ec65
hardikmishra30 <mishrahardik3006@gmail.com>
2024-12-20 15:05:53 +0530

    Arithmetic problem solved.

---
+++ DartProblems/arithmetic.dart
@@ -0,0 +1,16 @@
+void main() {
+  int a = 10;
+  int b = 5;
+
+  int sum = a + b;
+  print("Sum: $sum");
+
+  int diff = a - b;
+  print("Difference: $diff");
+
+  int product = a * b;
+  print("Product: $product");
+
+  int quotient = a ~/ b;
+  print("Quotient: $quotient");
+}
